c8609c2f2f65fdd391458fe841befe8fdf508d1e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 18:28:30 +0100

    Adding a new feature

--- discord-bot-tutorial/events/ready.js
+++ discord-bot-tutorial/events/ready.js
@@ -1,7 +1,9 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-    name: Events.ClientReady,
-    once: true,
-    execute
-}
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
