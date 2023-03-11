fe7905ea2c71c5f002a1f017e0f8815601894ee6
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 01:32:13 +0100

    Fixing a bug

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -1,8 +1,10 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-// const { token } = require('./config.json');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits] });
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+con
