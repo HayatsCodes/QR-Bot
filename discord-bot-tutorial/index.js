ccfb8ee11baf7ea7fba77574e3e992a642f89a5f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 01:34:15 +0100

    Updating the code

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -1,7 +1,7 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 // const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
