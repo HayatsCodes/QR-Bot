45921034426a24d2ddde5d588136a77e281c9878
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 21:52:28 +0100

    Updating the code

---
+++ discord-bot-tutorial/commands/echo.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input to echo back'));
+
+m
