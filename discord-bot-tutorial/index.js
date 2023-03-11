183231a5dbbbe65bc4b73f27ebb3c62bba486a7e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 11:51:51 +0100

    Improving performance

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -14,6 +14,8 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for 
+for (const file of commandFiles) {
+    
+}
 
 client.login(token);
