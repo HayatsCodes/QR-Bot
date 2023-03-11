a70f5664b8a1a0a8a083aab8a96282fb68b76d0e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 12:11:46 +0100

    Fixing a bug

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -19,7 +19,7 @@ for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	if ('data' in command && 'execute' in command) {
-		client.commands.endsWith(command.data.name, command);
+		client.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
