71450c2896d786a875e9e31d91a9d61ea46c43f2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 11:44:48 +0100

    Adding a new feature

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -9,6 +9,6 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.commands = new Coll
+client.commands = new Collection();
 
 client.login(token);
