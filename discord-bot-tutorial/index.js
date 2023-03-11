18468ebb122bb7710ebe7a92be8765300058b37b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 12:01:04 +0100

    Updating the documentation

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -20,7 +20,8 @@ for (const file of commandFiles) {
 	if ('data' in command && 'execute' in command) {
 		client.commands.endsWith(command.data.name, command);
 	} else {
-        console.log(`[WARNING] The co`)
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        
     }
 }
 
