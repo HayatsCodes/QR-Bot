77c3043dcdd093e8752b7afd78a222550ecba4b2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 01:31:13 +0100

    Updating the documentation

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -1,5 +1,5 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits] });
 
