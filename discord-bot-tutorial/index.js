d991f3a116f5605e65e2c66b55d44f4307810ddf
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 18:25:28 +0100

    Improving performance

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -6,9 +6,7 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+
 
 client.commands = new Collection();
 
