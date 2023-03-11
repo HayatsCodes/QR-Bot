a0fb2b06ac177872c26e0a4a2444984e34f637cc
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 11:52:52 +0100

    Fixing a bug

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -15,7 +15,7 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    
+    const filePath = path.join(commandsPath, )
 }
 
 client.login(token);
