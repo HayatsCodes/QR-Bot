1c243ff23961e30b6744f28c299e0bbb13861b49
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-11 11:53:52 +0100

    Updating the code

--- discord-bot-tutorial/index.js
+++ discord-bot-tutorial/index.js
@@ -15,7 +15,7 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, )
+	const filePath = path.join(commandsPath, file);
 }
 
 client.login(token);
