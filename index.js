58f239ec430a872ce0b8eadfde5d9bc0f046495d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 12:27:27 +0100

    Improving performance

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ for (const file of commandFiles) {
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
 }
 
