a7ed1164c21b1c7940ce690e1f4d95a94535d86a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 11:20:51 +0100

    Updating the code

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const config = {
-    TOKEN: process.env.TOKEN,
-    CLIENT_ID: process.env.CLIENT_ID,
-    GUILD_ID: process.env.GUILD_ID,
+	TOKEN: process.env.TOKEN,
+	CLIENT_ID: process.env.CLIENT_ID,
+	GUILD_ID: process.env.GUILD_ID,
 };
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
@@ -13,3 +13,4 @@ client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+client
