9f89e7684a95b8d11b7d0a994eae16a9a59dbeae
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 11:55:29 +0100

    Improving performance

---
+++ commands/generate_qr.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+	.setName('generate')
+	.setDescription('generates qr code')
+	.addStringOption(option => {
+		option.setName('input')
+			.setDescription('The characters you want to embed in the QR code')
+			.setRequired(true);
+	});
+const execute = async function execute(interaction) {
+	const input = interaction.options.getString('input');
+};
