cac5603beeba00e260e4ce652e8816713a1e2c2c
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 12:38:07 +0100

    Updating the documentation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable brace-style */
 const fs = require('fs');
 const path = require('path');
-const { Client, Events, GatewayIntentBits, Collection, Events } = require('discord.js');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 require('dotenv').config();
 
 const config = {
@@ -32,26 +32,26 @@ for (const file of commandFiles) {
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+	const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
+	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found`);
 		return;
 	}
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
 			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
 		} else {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
-    }
-    console.log(interaction);
+	}
+	console.log(interaction);
 });
 
 client.login(config.TOKEN);
