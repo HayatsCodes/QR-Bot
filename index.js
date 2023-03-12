c1651127cd574ffd3c18571afe1c3b65e9c0161f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 11:19:50 +0100

    Improving performance

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 const config = {
-    TOKEM
+    TOKEN: process.env.TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    GUILD_ID: process.env.GUILD_ID,
 };
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
