36eb1f096eb25800b7c66284e53d458a19be5c24
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 12:37:07 +0100

    Fixing a bug

--- index.js
+++ index.js
@@ -45,7 +45,13 @@ client.on(Events.InteractionCreate, async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
+        if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
     }
+    console.log(interaction);
 });
 
 client.login(config.TOKEN);
