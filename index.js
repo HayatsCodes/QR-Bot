88f07088e097cd2a4e594bef1eccd3b2584287d1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 11:18:49 +0100

    Updating the documentation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
 
+const config = {
+    TOKEM
+};
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
 client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
