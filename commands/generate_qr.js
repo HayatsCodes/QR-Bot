e3adcd473fd9d4bcde2747dad6398ac61e82b435
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 15:18:43 +0100

    Fixing a bug

--- commands/generate_qr.js
+++ commands/generate_qr.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 const getQR = require('../utils/get_qr');
 
 const data = new SlashCommandBuilder()
