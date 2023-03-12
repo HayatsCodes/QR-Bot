9d56532f4267ff5bf214bde7d0dc5275dfa61fb2
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 18:39:11 +0100

    Improving performance

--- utils/get_qr.js
+++ utils/get_qr.js
@@ -1,7 +1,14 @@
 const QRCode = require('qrcode');
 
 async function getQR(userInput) {
-    const qrData = userInput;
+	const QRData = userInput;
+	const QROptions = {
+		errorCorrectionLevel: 'H',
+		type: 'png',
+		rendererOpts: {
+			quality: 0.3,
+		},
+	};
 }
 
 module.exports = getQR;
