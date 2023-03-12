58009074e554165124c8a8bc4151ef86fe9569f9
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 14:35:12 +0100

    Improving performance

--- utils/get_qr.js
+++ utils/get_qr.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 
 async function getQR(userInput) {
 	try {
-		const response = await axios.get(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${userInput}`);
+		const response = await axios.get(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${userInput}`, {
+            
+        });
 		console.log(response.data);
-		return response.data;
 	} catch (error) {
 		console.log(error);
 	}
-
 }
 
 module.exports = getQR;
