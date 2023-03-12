c629a3949b305d758abda4ad46a7867d22eea454
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 11:17:49 +0100

    Refactoring the code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
+
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
 client.once(Events.ClientReady, (c) => {
