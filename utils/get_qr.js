3cdfb760d03d0153978d73fb9d0de22e556241d7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 14:33:11 +0100

    Adding a new feature

--- utils/get_qr.js
+++ utils/get_qr.js
@@ -3,13 +3,12 @@ const axios = require('axios');
 
 async function getQR(userInput) {
 	try {
-		const response = await axios.get(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${userInput}`, {
-			responseType: ArrayBuffer,
-		});
+		const response = await axios.get(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${userInput}`);
 		console.log(response.data);
 	} catch (error) {
 		console.log(error);
 	}
+    return r
 }
 
 module.exports = getQR;
