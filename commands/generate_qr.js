5ae9724e7250a2b6e8524554c3f583c82a12ec20
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 19:25:52 +0100

    Fixing a bug

--- commands/generate_qr.js
+++ commands/generate_qr.js
@@ -16,7 +16,7 @@ const execute = async function execute(interaction) {
 
 	await QRGenerator(input);
 
-    const file = new AttachmentBuilder('./')
+	const file = new AttachmentBuilder('./qr-code.png');
 
 	// const QREmbed = new EmbedBuilder()
 	// 	.setTitle('QR Code')
