78ba07039a6dec074853a05e37096661d31ef199
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-12 18:22:30 +0100

    Updating the code

--- utils/get_qr.js
+++ utils/get_qr.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 async function getQR(userInput) {
 	try {
 		const response = await axios.get(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${userInput}`);
+        const QRData =
 
 		// const QRImageUrl = `data:image/png;base64,${QRData}`;
 
