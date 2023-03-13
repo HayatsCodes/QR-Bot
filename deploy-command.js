4e8449f2e13027b61773b026d6fdd1025e12c55e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-13 12:27:18 +0100

    Improving performance

--- deploy-command.js
+++ deploy-command.js
@@ -1,38 +1,24 @@
-/* eslint-disable brace-style */
-const { REST, Routes } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-const config = require('./index');
+// your code here
 
-
-const commands = [];
-// Grab all the command files from the commands directory
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(config.TOKEN);
-
-// and deploy your commands!
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	  console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  
+	  // The put method is used to fully refresh all commands in the guild with the current set
+	  const data = await rest.put(
+		Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
+		{ body: commands },
+	  );
+  
+	  console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
+	  // And of course, make sure you catch and log any errors!
+	  console.error(error);
 	}
-})();
+  })();
+  
+  // Stop the IIFE after 30 seconds
+  setTimeout(() => {
+	return;
+  }, 30000);
+  
