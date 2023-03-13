bc037ed190f57b97d8f267ab8b085f2a8410fc2b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-13 20:47:32 +0100

    Updating the documentation

--- deploy-command.js
+++ deploy-command.js
@@ -26,7 +26,7 @@ const rest = new REST({ version: '10' }).setToken(config.TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(config.CLIENT_ID, config.GUILD_ID),
+			Routes.applicationCommands(config.CLIENT_ID, config.GUILD_ID),
 			{ body: commands },
 		);
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
