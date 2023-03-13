9c0e3c2942fce4321032d3cf83cd1002ea06e8f7
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-13 20:59:12 +0100

    Adding a new feature

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
 	const command = interaction.client.commands.get(interaction.commandName);
-	co
+	console.log('dev: ', interaction.commandName);
 
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found`);
