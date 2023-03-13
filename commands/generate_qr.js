68447999edecb32ae47d0d951078f2cd93f122a1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-13 01:03:55 +0100

    Fixing a bug

--- commands/generate_qr.js
+++ commands/generate_qr.js
@@ -30,7 +30,7 @@ const data = new SlashCommandBuilder()
 				{ name: 'Red', value: '#FF0000' },
 				{ name: 'Green', value: '#00FF00' },
 				{ name: 'Blue', value: '#0000FF' },
-				{ name: 'Yellow', value: '#FFFF00' },
+				{ name: 'Yellow', value: '#FFFF00'},
 			);
 	});
 
