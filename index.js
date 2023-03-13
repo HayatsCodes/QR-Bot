39e34b774d1f6798152bc3e97c1b2c5a4b800e24
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-13 20:48:34 +0100

    Fixing a bug

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('dotenv').config();
 const config = {
 	TOKEN: process.env.TOKEN,
 	CLIENT_ID: process.env.CLIENT_ID,
-	GUILD_ID: process.env.GUILD_ID,
 };
 
 // Instantiate a new Client instance
