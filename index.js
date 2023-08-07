6ae88f959d26fee2ca80da7553b58ff18b06fb5d
Hayats <hayatudeenabdulrahman@gmail.com>
2023-08-07 02:28:51 +0100

    production ready

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-/* eslint-disable brace-style */
 const fs = require('fs');
 const path = require('path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 require('dotenv').config();
-
+const express = require('express');
 
 const config = {
-	TOKEN: process.env.TOKEN,
-	CLIENT_ID: process.env.CLIENT_ID,
+  TOKEN: process.env.TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
 };
+
+const app = express();
+const PORT = 3000;
 
 // Instantiate a new Client instance
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
-console.log('client ', client);
-
 // Fire the event when logged in to discord successfully
 client.once(Events.ClientReady, (c) => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // Create a collection of commands
@@ -28,37 +28,42 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 
 // loop through every file in commandFiles
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Check if data and execute module exists in each file
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Check if data and execute module exists in each file
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+  }
 }
 
 // Fire the event when user interacts with the bot
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`HTTP server listening on port ${PORT}`);
 });
 
 client.login(config.TOKEN);
